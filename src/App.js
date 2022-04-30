c1561888b0360548cd504d387f3676f8acefa15c
lidor4x4 <61459392+lidor4x4@users.noreply.github.com>
2022-04-30 10:42:45 +0300

    Coded the first version of the webapp.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import {useRef, useState} from 'react';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+export default function App(){
+  const [idea, setIdea] = useState("");
+  const ideaTextRef = useRef();
+  // array and not db/json file because it's only a prototype of the webapp
+  const ideas = [" Travel Log App","Personal Project Management Tool","Public Blog App"," E-Commerce Website/Shopify Clone", "Bookstore Library & Stock keeping app", "Social Media App for local handicrafts", "Task Management Tool", "Workout Tracker App", "Realtime Chat App", "Hospital Management App"];
+  function handleClick(){
+    setIdea(ideas[Math.floor(Math.random() * ideas.length)])
+    ideaTextRef.current.classList.remove("hide");
+  }
+  
+  return(
+    <div className="home-container">
+      <h1 className="title">Generate MERN project idea</h1>
+      <button onClick={handleClick}>Generate</button>
+      <h2 ref={ideaTextRef} className="hide">{idea}</h2>
     </div>
-  );
+  )
 }
-
-export default App;
